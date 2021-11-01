903e1da3bcc0b05ded8eb98106c56314d1e57e59
Monika Geiger <geigermonika@hotmail.hu>
2021-11-01 12:00:28 +0100

    basic vue app

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button :style="{background:color}">{{text}}</button>
+</template>
+
+<script>
+export default{
+    name:'Button',
+    props:{
+        text:String,
+        color:String
+        }
+
+    }
+</script>
